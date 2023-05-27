4b320d96e8c9a9c23c0868fc66d7d1172aff9149
dg1223 <alavi1223@hotmail.com>
2023-05-27 18:29:13 -0400

    dashboard side panel

---
+++ src/Layout/Dashboard.jsx
@@ -0,0 +1,77 @@
+import {
+  FaCalendarAlt,
+  FaEnvelope,
+  FaHamburger,
+  FaHome,
+  FaShoppingBag,
+  FaShoppingCart,
+  FaWallet,
+} from "react-icons/fa";
+import { Link, Outlet } from "react-router-dom";
+
+const Dashboard = () => {
+  return (
+    <div className="drawer drawer-mobile">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col items-center justify-center">
+        <Outlet></Outlet>
+        {/* Page content here */}
+        <label
+          htmlFor="my-drawer-2"
+          className="btn btn-primary drawer-button lg:hidden"
+        >
+          Open drawer
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+        <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+          {/* Sidebar content here */}
+          <li>
+            <Link>
+              <FaHome /> User Home
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <FaCalendarAlt /> Reservations
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <FaWallet /> Payment History
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <FaShoppingCart /> My Cart
+            </Link>
+          </li>
+          <div className="divider"></div>
+          <li>
+            <Link to="/">
+              <FaHome /> Home
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <FaHamburger /> Menu
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <FaShoppingBag /> Shop
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <FaEnvelope /> Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
         <Link to="/secret">Secret</Link>
       </li>
       <li>
-        <Link to="/">
+        <Link to="/dashboard/mycart">
           <button className="btn gap-2">
             <FaShoppingCart />
             <div className="badge badge-secondary">{cart?.length || 0}</div>
