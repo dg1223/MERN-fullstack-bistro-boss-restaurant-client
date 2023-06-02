e55894dfd05f5582455cafe8c3e367b3c5db18eb
dg1223 <alavi1223@hotmail.com>
2023-06-03 05:49:53 +0600

    stripe configuration

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,10 +1,18 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import CheckoutForm from "./CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe(import.meta.env.VITE_paymentGatewayPK);
 
 const Payment = () => {
   return (
     <div>
       <SectionTitle subHeader="Pleae process" header="Payment"></SectionTitle>
       <h2 className="text-3xl">Give me money</h2>
+      <Elements stripe={stripePromise}>
+        <CheckoutForm></CheckoutForm>
+      </Elements>
     </div>
   );
 };
