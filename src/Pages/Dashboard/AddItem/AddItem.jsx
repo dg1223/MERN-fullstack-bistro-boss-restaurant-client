4c2431d2dd420d0b7369c2422d7c9fd7b3e18e3e
dg1223 <alavi1223@hotmail.com>
2023-06-02 13:53:17 +0600

    react hook form for add item

--- src/Pages/Dashboard/AddItem/AddItem.jsx
+++ src/Pages/Dashboard/AddItem/AddItem.jsx
@@ -1,7 +1,82 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { useForm } from "react-hook-form";
+
 const AddItem = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  console.log(errors);
+
   return (
-    <div>
-      <h2 className="text-4xl">Add an item</h2>
+    <div className="w-full px-10">
+      <SectionTitle subHeader="What's new" header="Add an Item"></SectionTitle>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text font-semibold">Recipe name*</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Recipe name"
+            {...register("name", { required: true, maxLength: 120 })}
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text font-semibold">Category*</span>
+          </label>
+          <select
+            {...register("category", { required: true })}
+            className="select select-bordered"
+          >
+            <option disabled selected>
+              Pick one
+            </option>
+            <option>Pizza</option>
+            <option>Soup</option>
+            <option>Salad</option>
+            <option>Drinks</option>
+            <option>Dessert</option>
+          </select>
+        </div>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text font-semibold">Price*</span>
+          </label>
+          <input
+            type="number"
+            placeholder="Type here"
+            {...register("price", { required: true })}
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Recipe details</span>
+          </label>
+          <textarea
+            {...register("details", { required: true })}
+            className="textarea textarea-bordered h-24"
+            placeholder="Recipe details"
+          ></textarea>
+        </div>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Item image*</span>
+          </label>
+          <input
+            type="file"
+            className="file-input file-input-bordered w-full max-w-xs"
+          />
+        </div>
+        <input className="btn btn-sm mt-4" type="submit" value="Add Item" />
+      </form>
     </div>
   );
 };
