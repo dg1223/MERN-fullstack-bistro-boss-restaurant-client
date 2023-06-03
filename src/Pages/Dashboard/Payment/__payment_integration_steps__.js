557da9141c409a4857de55403856882841e75fb8
dg1223 <alavi1223@hotmail.com>
2023-06-03 07:11:19 +0600

    create payment intent

---
+++ src/Pages/Dashboard/Payment/__payment_integration_steps__.js
@@ -0,0 +1,11 @@
+/**
+ * 1. install stripe and react stripe JS
+ * 2. create a checkout form (card element contains: card number, expiration date, cvv)
+ * 3. create account on stripe and get the publishable key (pk)
+ * 4. get card information
+ * 5. create a payment method
+ * 6. use test card to test payment
+ * 7. on the server side, install stripe
+ * 8. create a payment intent with payment method types: ['card']
+ *    make sure you provide amount in cents -> multiply by 100
+ */
