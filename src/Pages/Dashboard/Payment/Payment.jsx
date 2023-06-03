add19739ecbf90d36c0a5cf357466dbb7ccc170f
dg1223 <alavi1223@hotmail.com>
2023-06-03 07:29:20 +0600

    calculate cart total on payment page

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -2,16 +2,22 @@ import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import CheckoutForm from "./CheckoutForm";
 import { Elements } from "@stripe/react-stripe-js";
+import useCart from "../../../hooks/useCart";
 
 const stripePromise = loadStripe(import.meta.env.VITE_paymentGatewayPK);
 
 const Payment = () => {
+  const [cart] = useCart();
+
+  const total = cart.reduce((sum, item) => sum + item.price, 0);
+  const price = parseFloat(total.toFixed(2));
+
   return (
     <div>
       <SectionTitle subHeader="Pleae process" header="Payment"></SectionTitle>
       <h2 className="text-3xl">Give me money</h2>
       <Elements stripe={stripePromise}>
-        <CheckoutForm></CheckoutForm>
+        <CheckoutForm price={price}></CheckoutForm>
       </Elements>
     </div>
   );
